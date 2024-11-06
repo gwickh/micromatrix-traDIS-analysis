5a50da41440cba59a29c0f35e9452462cfbe9d6a
gwickh <gregory.wickham@quadram.ac.uk>
2024-11-06 16:33:33 +0000

    utility sctipt to loop albatradis through multiple directories

---
+++ shell_scripts/albatradis.sh
@@ -0,0 +1,37 @@
+#!/usr/bin/env bash
+#author:    :Gregory Wickham
+#date:      :20241106
+#version    :1.0.0
+#desc       :Script to run albatradis
+#usage		:bash albatradis.sh <EMBL file> <dir containing plotfiles>
+#===========================================================================================================
+echo "finding conda base environment"
+source "$(sudo find ~ -maxdepth 4 -name conda.sh)" #find path to conda base environment
+echo "conda base environment located"
+
+echo "finding albatradis conda environment"
+envpath="$(sudo find ~ -maxdepth 3 -name envs)" #check if albatradis conda env exists
+if [ -d $envpath/albatradis ] 
+then
+	echo "albatradis conda env present" 
+else
+	echo "creating conda env: quatradis" 
+	conda create -y -n albatradis -c bioconda -c conda-forge albatradis
+fi
+
+conda activate albatradis
+for k in $2/*
+do
+    #check if text file with plotfile filenames exists
+    if [ -f $k/$(basename $k).txt ]
+    then
+        #if so, then delete
+        rm $k/$(basename $k).txt
+    fi
+    #create text file with plotfile filenames
+    ls -d $k/* > $k/.$(basename $k).txt && mv $k/.$(basename $k).txt $k/$(basename $k).txt
+    #pass filenames to albatradis
+    xargs \
+        -a $k/$(basename $k).txt \
+        albatradis -a $1
+done
