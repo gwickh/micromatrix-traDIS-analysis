5dccab80a3ca3b2dfb25f21f03d3cdd3007610b0
gwickh <gregory.wickham@quadram.ac.uk>
2024-11-11 11:16:03 +0000

    init

---
+++ python_scripts/__init__.py
@@ -0,0 +1,5 @@
+# micoromatrix-traDIS-analysis/python_scipts/__init__.py
+__version__ = "1.0.0"
+__author__ = "Gregory Wickham"
+
+from .plotting_functions import plot_volcano
--- python_scripts/plotting_functions/__init__.py
+++ python_scripts/plotting_functions/__init__.py
@@ -1,3 +1,5 @@
 # micoromatrix-traDIS-analysis/python_scipts/plotting_functions/__init__.py
 __version__ = "1.0.0"
 __author__ = "Gregory Wickham"
+
+from .plot_volcano import plot_volcano
