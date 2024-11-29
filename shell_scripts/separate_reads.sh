f32bcfb9909bdf4bbf14368e9d60d0b948b69a2d
gwickh <gregory.wickham@quadram.ac.uk>
2024-11-29 15:31:52 +0000

    loop through .bam files

--- shell_scripts/separate_reads.sh
+++ shell_scripts/separate_reads.sh
@@ -7,11 +7,22 @@
 #===========================================================================================================
 
 #populate barcodes.txt file which contains a list of barcodes from demultiplexed .bam file
-samtools view reads_cat_minion.bam | \
-    awk '{for(i=12;i<=NF;i++) if($i ~ /^BC:Z:/) print substr($i,6)}' | \
-        sort | \
-            uniq > barcodes.txt
 
-while read barcode; do
-    samtools view -b -h reads_cat_minion.bam -e "BC:Z:${barcode}" -o "${barcode}.bam"
-done < barcodes.txt
+source "$(sudo find ~ -maxdepth 4 -name conda.sh)" #find path to conda base environment
+echo "conda sourced"
+conda activate samtools
+
+for bam in *.bam; do
+    echo "Processing $bam..."  # Check which BAM file is being processed
+    samtools view $bam | \
+        awk '{for(i=12;i<=NF;i++) if($i ~ /^BC:Z:/) print substr($i,6)}' | \
+        sort | \
+        uniq > barcodes.txt
+    echo "Barcodes for $bam written to barcodes.txt"
+
+    while read barcode; do
+        samtools view -h $bam | \
+            awk -v bc=$barcode '$0 ~ /^@/ || $0 ~ "BC:Z:"bc {print}' | \
+                samtools view -bo "${barcode}.bam" -
+    done < barcodes.txt
+done
